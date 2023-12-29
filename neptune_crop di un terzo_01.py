7b67653ee7fb663fa91c0c183cc65861d97fce67
Doceo <diomede.mazzone@gmail.com>
2023-12-29 11:55:42 +0100

    nepture crop

---
+++ neptune_crop di un terzo_01.py
@@ -0,0 +1,21 @@
+import cv2
+
+def Crop_image(image, percent):
+  width = image.shape[1]
+  height = image.shape[0]
+
+  # Calcola le dimensioni del ritaglio.
+  crop_width = int(width * (percent))
+  crop_height = int(height * (percent))
+ # Ritaglia l'immagine.
+  crop_image = image[crop_height:-crop_height, crop_width:-crop_width]  
+  return crop_image
+
+# Carica l'immagine.
+image = cv2.imread("photo_0683.jpg")
+
+# Ritaglia l'immagine del 30%.
+crop_image = Crop_image(image, 0.33)
+# Mostra l'immagine ritagliata.
+cv2.imshow("Image", crop_image)
+cv2.waitKey(0)
